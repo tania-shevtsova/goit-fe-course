76b583e26dd05cb2cb789156f3db9c15d9282050
Tania <taniameoow@gmail.com>
2019-12-11 18:51:58 +0200

    remove file

---
+++ javascript/module-07/task1.js
@@ -0,0 +1,9 @@
+const list = Array.from(document.querySelector("ul#categories").children);
+console.log(`В списке ${list.length} категории!`);
+
+list.forEach(elem => {
+  const title = elem.querySelector("h2");
+  const child = elem.querySelector("ul").children.length;
+  console.log(`Категория: ${title.innerText}`);
+  console.log(`Количество элементов: ${child}`);
+});
---
+++ javascript/module-07/task2.js
@@ -0,0 +1,20 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+
+  const add = function(arr) {
+      arr.forEach(elem => {
+          const list = document.querySelector('ul#ingredients');
+          const item=document.createElement('li');
+          list.appendChild(item);
+          item.innerHTML = elem;
+
+      })
+  }
+
+  add(ingredients);
