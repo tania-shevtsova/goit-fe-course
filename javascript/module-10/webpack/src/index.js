f5cf9e80bd5c8c33caefab2758d148a70daf4859
Tania <taniameoow@gmail.com>
2019-12-19 15:20:48 +0200

    add some changes

--- javascript/module-10/webpack/src/index.js
+++ javascript/module-10/webpack/src/index.js
@@ -1,54 +1,47 @@
 import menu from './menu.json';
 import './styles.css';
-import smth from './templates/item.hbs'
-
+import smth from './templates/item.hbs';
 
 const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-const theme=document.querySelector('input.js-switch-input');
-const body=document.querySelector('body');
-body.classList.add(Theme.LIGHT);
-const local=localStorage.getItem('Theme');
-const check=document.querySelector('input[type="checkbox"]')
+const check = document.querySelector('input[type="checkbox"]');
 
-if(local==='DARK'){
-    body.classList.add(Theme.DARK);
-    check.checked=true;
+const theme = document.querySelector('input.js-switch-input');
+const body = document.querySelector('body');
+
+const local = localStorage.getItem('Theme');
+
+if (local === 'DARK') {
+  body.classList.add(Theme.DARK);
+  check.checked = true;
+} else {
+  body.classList.add(Theme.LIGHT);
+  check.checked = false;
 }
 
+theme.addEventListener('change', e => {
+  if (body.classList.contains(Theme.LIGHT)) {
+    localStorage.setItem('Theme', 'DARK');
+    body.classList.add(Theme.DARK);
+    body.classList.remove(Theme.LIGHT);
+  } else if (body.classList.contains(Theme.DARK)) {
+    localStorage.removeItem('Theme', 'LIGHT');
 
-theme.addEventListener('change', (e)=> {
-
-    if (body.classList.contains(Theme.LIGHT)) {
-        localStorage.setItem('Theme', 'DARK');  
-        body.classList.add(Theme.DARK);
-        body.classList.remove(Theme.LIGHT);
-
-    }
-
-    else if (body.classList.contains(Theme.DARK)) {
-    localStorage.removeItem('Theme', 'LIGHT'); 
-
-       body.classList.remove(Theme.DARK);
-        body.classList.add(Theme.LIGHT);
-    }
+    body.classList.remove(Theme.DARK);
+    body.classList.add(Theme.LIGHT);
+  }
 });
 
-
-const refs={
-    itemM: document.querySelector('.js-menu')
-}
+const refs = {
+  itemM: document.querySelector('.js-menu'),
+};
 
 buildPost(menu);
 
-function buildPost(menu){
-    const markup=menu.map(men => smth(men)).join('');
-   refs.itemM.insertAdjacentHTML('beforeend', markup);
-
+function buildPost(menu) {
+  const markup = menu.map(men => smth(men)).join('');
+  refs.itemM.insertAdjacentHTML('beforeend', markup);
 }
-
-
-  
