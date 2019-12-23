6f3dbd4f574218360409c2339f97c7babc48fe34
Tania <taniameoow@gmail.com>
2019-12-23 19:02:09 +0200

    add some changes

--- javascript/module-08/index.js
+++ javascript/module-08/index.js
@@ -1,41 +1,42 @@
 import arr from "./gallery-items.js";
 
-const ul = document.querySelector(".js-gallery");
-
-const div = document.querySelector("div.lightbox");
-const img = document.querySelector("div.lightbox__content > .lightbox__image");
-
 const refs = {
+  ul: document.querySelector(".js-gallery"),
+  div: document.querySelector("div.lightbox"),
+  img: document.querySelector("div.lightbox__content > .lightbox__image"),
   content: document.querySelector(".lightbox__content"),
-  overlay: document.querySelector(".lightbox__overlay")
+  overlay: document.querySelector(".lightbox__overlay"),
+  closeBtn: document.querySelector('button[data-action="close-lightbox"]')
 };
+
 refs.content.addEventListener("click", handleRefs);
 refs.overlay.addEventListener("click", handleRefs);
+refs.ul.addEventListener("click", handleListClick);
+refs.closeBtn.addEventListener("click", handleListClose);
+document.addEventListener("keydown", handleKeyPress);
+
 
 const create = function(arr) {
   const string = arr.reduce((acc, element) => {
     return (acc += `<li><a class="gallery__link" href=${element.original}><img class="gallery__image" src=${element.preview} data-source=${element.original} alt=${element.description}></li>`);
   }, "");
-  ul.insertAdjacentHTML("afterbegin", string);
+  refs.ul.insertAdjacentHTML("afterbegin", string);
 };
-ul.addEventListener("click", handleListClick);
+
 create(arr);
 
 function handleListClick(e) {
   e.preventDefault();
-  div.classList.add("is-open");
-  img.src = e.target.dataset.source;
-  window.addEventListener("keydown", handleKeyDown);
+  refs.div.classList.add("is-open");
+  refs.img.src = e.target.dataset.source;
+  window.addEventListener("keydown", handleKeyPress);
 }
 
-const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
-
-closeBtn.addEventListener("click", handleListClose);
 
 function handleListClose(e) {
-  div.classList.remove("is-open");
-  window.removeEventListener("keydown", handleKeyDown);
-  img.src = "";
+  refs.div.classList.remove("is-open");
+  window.removeEventListener("keydown", handleKeyPress);
+  refs.img.src = "";
 }
 
 function handleRefs(e) {
@@ -46,15 +47,14 @@ function handleRefs(e) {
   handleListClose();
 }
 
-function handleKeyDown(e) {
-  if (event.code !== "Escape") {
-    return;
-  }
+// function handleKeyDown(e) {
+//   if (event.code !== "Escape") {
+//     return;
+//   }
 
-  handleListClose();
-}
+//   handleListClose();
+// }
 
-document.addEventListener("keydown", handleKeyPress);
 function handleKeyPress(e) {
   if (e.code === "Escape") {
     const divKey = document.querySelector("div.lightbox");
